985e940aaf9cd17b1aa58e0232fbc5b028e134ae
xibin <xibin@ue.com>
2017-08-09 10:28:24 +0800

    优化配置

--- config/src/main/resources/import.sql
+++ config/src/main/resources/import.sql
@@ -10,4 +10,17 @@ CREATE TABLE `demo_user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='user table';
 
-insert into demo_user (user_name, sex, phone, address, reg_time) values ('xibin', 0 , '5523380', 'ChengDu City', '2017-08-08');
+insert into demo_user (user_name, sex, phone, address, reg_time) values ('xibin', 0 , '10000', 'ChengDu City', '2017-08-08');
+insert into demo_user (user_name, sex, phone, address, reg_time) values ('xibin1', 0 , '10000', 'ChongQing City', '2017-08-08');
+insert into demo_user (user_name, sex, phone, address, reg_time) values ('xibin2', 0 , '10000', 'BeiJing City', '2017-08-08');
+insert into demo_user (user_name, sex, phone, address, reg_time) values ('xibin3', 0 , '10000', 'GuangZhou City', '2017-08-08');
+insert into demo_user (user_name, sex, phone, address, reg_time) values ('xibin4', 0 , '10000', 'TianJin City', '2017-08-08');
+insert into demo_user (user_name, sex, phone, address, reg_time) values ('xibin5', 0 , '10000', 'WuHan City', '2017-08-08');
+insert into demo_user (user_name, sex, phone, address, reg_time) values ('xibin6', 0 , '10000', 'XiAn City', '2017-08-08');
+insert into demo_user (user_name, sex, phone, address, reg_time) values ('xibin7', 0 , '10000', 'ShangHai City', '2017-08-08');
+insert into demo_user (user_name, sex, phone, address, reg_time) values ('xibin8', 0 , '10000', 'NanChong City', '2017-08-08');
+insert into demo_user (user_name, sex, phone, address, reg_time) values ('xibin9', 0 , '10000', 'PaoTing City', '2017-08-08');
+insert into demo_user (user_name, sex, phone, address, reg_time) values ('xibin10', 0 , '10000', 'WuLuMuQi City', '2017-08-08');
+insert into demo_user (user_name, sex, phone, address, reg_time) values ('xibin11', 0 , '10000', 'ChengDu City', '2017-08-08');
+insert into demo_user (user_name, sex, phone, address, reg_time) values ('xibin12', 0 , '10000', 'ChengDu City', '2017-08-08');
+insert into demo_user (user_name, sex, phone, address, reg_time) values ('xibin13', 0 , '10000', 'ChengDu City', '2017-08-08');
