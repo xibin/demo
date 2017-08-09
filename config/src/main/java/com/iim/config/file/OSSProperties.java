71974cc3c9e4b8a1a992fbfd4e488eadf3927bd8
xibin <xibin@ue.com>
2017-08-09 16:17:20 +0800

    优化数据源配置。

---
+++ config/src/main/java/com/iim/config/file/OSSProperties.java
@@ -0,0 +1,33 @@
+package com.iim.config.file;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by xibin on 2017/8/9.
+ */
+@Component
+@ConfigurationProperties("oss")
+public class OSSProperties {
+
+    private String bucketid;
+
+    private String basepath;
+
+    public String getBucketid() {
+        return bucketid;
+    }
+
+    public void setBucketid(String bucketid) {
+        this.bucketid = bucketid;
+    }
+
+    public String getBasepath() {
+        return basepath;
+    }
+
+    public void setBasepath(String basepath) {
+        this.basepath = basepath;
+    }
+
+}
